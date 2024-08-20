de8cea6df93b8821133b73659ee5ebfecf933f98
Christian <christianponce04@gmail.com>
2024-08-20 18:27:20 +0800

    post and get tutorial

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
-<?php 
-
-echo 'hello world, php first echo';
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP tutorial</title>
+    <title>Get and Post Tutorial</title>
 </head>
 <body>
+
+    <form action="index.php" method="GET">
+        <input type="text" name="username" id="" placeholder="Username">
+        <input type="password" name="password" id="" placeholder="Password">
+        <input type="submit" value="Log In">
+
+    </form>
     
 </body>
 </html>
+
+
+<?php 
+
+echo   "{$_GET["username"]} </br> ";
+echo   $_GET["password"];
+
+?>
