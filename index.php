c93db5fcd9a088b47fd0e0b6b86f4b2a2eb6c2fc
Christian <christianponce04@gmail.com>
2024-08-30 17:48:11 +0800

    if else exercise

--- index.php
+++ index.php
@@ -6,14 +6,30 @@
     <title>If Else Condition</title>
 </head>
 <body>
+
+    <form action="index.php" method="post">
+
+    <input type="text" name="age" id="" placeholder="Enter Age">
+    <input type="submit" value="submit">
+    </form>
     
 </body>
 </html>
 
 <?php 
 
-    
+    $age = $_POST["age"];
 
+    if($age>=18){
+        echo "allowed to come in, age is {$age}";
+    }
+    else if($age <=0){
+        echo "invalid input";
+    }
+    else{
+        echo "Not Allowed still minor, age is {$age}";
+    }
+   
 
 
 ?>
