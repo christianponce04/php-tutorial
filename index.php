7c30bcf1c0e5f267cf1fc0aedeb950d970811f50
Christian <christianponce04@gmail.com>
2024-09-03 16:46:09 +0800

    switch exercises

--- index.php
+++ index.php
@@ -3,44 +3,70 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>While Loop Condition</title>
+    <title>Exercises</title>
 </head>
 <body>
 
-<!-- exercise stop watch  stop timer if button is click-->
+<!-- Write a simple calculator program in PHP using switch case  -->
 
-    <form action="index.php" method="post">
-        <input type="submit" name="stop" value="STOP">
-    </form>
+    <form action="index.php" method="post" >
+
+        <input type="number" name="first_num" id="" placeholder= "Enter First Number:">
+        <input type="number" name="second_num" id="" placeholder= "Enter Second Number:">
+
+        <input type="submit" value="Add" name="operation">
+        <input type="submit" value="Subtract" name="operation">
+        <input type="submit" value="Multiplication" name="operation">
+        <input type="submit" value="Division" name="operation">
+        <input type="submit" value="modulo" name="operation"> </br>
+
+        <input type="text" value="<?php echo $result; ?>"> </br>
+
+        
+	  </form>
 
 </body>
 </html>
 
-<?php 
+<?php
 
-// $counter = 0;
-
-// while($counter < 10){
-
-//     $counter ++;
-//     echo $counter . "</br>";
-// }
-
-$counter =10;
-$timer = true;
+$first_num = $_POST["first_num"];
+$second_num = $_POST["second_num"];
+$operation = $_POST["operation"];
+$result = null;
 
 
-while($timer){
+switch($operation){
 
-    if(isset($_POST["stop"])){
-        $timer == false;
+    case "Add":
 
-    }
-    else{
-         $counter++;
-        echo $counter . "</br>";
-    }
+        $result = $first_num + $second_num;
+        echo $result;
+        break;
+
+    case "Subtract":
+        $result = $first_num - $second_num;
+        echo $result;
+        break;
+
+     case "Multiplication":
+            $result = $first_num * $second_num;
+            echo $result;
+            break;
+
+     case "Division":
+            $result = $first_num / $second_num;
+            echo $result;
+            break;
+
+     case "modulo":
+            $result = $first_num % $second_num;
+            echo $result;
+            break;
+
+        default:
+        echo "Please select operations";
+        break;
+
 }
-
-
 ?>
