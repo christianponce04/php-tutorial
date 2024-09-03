c8af089fd83dfbb48b2d46ce78ae40f74e471040
Christian <christianponce04@gmail.com>
2024-09-03 17:50:09 +0800

    exercise 2

--- index.php
+++ index.php
@@ -5,68 +5,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercises</title>
 </head>
-<body>
 
-<!-- Write a simple calculator program in PHP using switch case  -->
-
-    <form action="index.php" method="post" >
-
-        <input type="number" name="first_num" id="" placeholder= "Enter First Number:">
-        <input type="number" name="second_num" id="" placeholder= "Enter Second Number:">
-
-        <input type="submit" value="Add" name="operation">
-        <input type="submit" value="Subtract" name="operation">
-        <input type="submit" value="Multiplication" name="operation">
-        <input type="submit" value="Division" name="operation">
-        <input type="submit" value="modulo" name="operation"> </br>
-
-        <input type="text" value="<?php echo $result; ?>"> </br>
-
-        
-	  </form>
-
-</body>
-</html>
 
 <?php
 
-$first_num = $_POST["first_num"];
-$second_num = $_POST["second_num"];
-$operation = $_POST["operation"];
+$day = $_POST["day"];
 $result = null;
 
-
-switch($operation){
-
-    case "Add":
-
-        $result = $first_num + $second_num;
-        echo $result;
+switch ($day) {
+    case "1":
+        $result = "It is Monday!";
         break;
-
-    case "Subtract":
-        $result = $first_num - $second_num;
-        echo $result;
+    case "2":
+        $result = "It is today!";
         break;
-
-     case "Multiplication":
-            $result = $first_num * $second_num;
-            echo $result;
-            break;
-
-     case "Division":
-            $result = $first_num / $second_num;
-            echo $result;
-            break;
-
-     case "modulo":
-            $result = $first_num % $second_num;
-            echo $result;
-            break;
-
-        default:
-        echo "Please select operations";
+    case "3":
+        $result = "It is Wednesday!";
         break;
-
+	case "4":
+        $result = "It is Thursday!";
+        break;
+    case "5":
+        $result = "It is Friday!";
+        break;
+    case "6":
+        $result = "It is Saturday!";
+        break;
+	case "7":
+        $result = "It is Sunday!";
+        break;
+    default:
+        $result = "Invalid number!";
 }
+
 ?>
+
+<body>
+
+<!-- Write a program to show day of the week (for example: Monday) based on numbers using switch/case statements. -->
+ 
+<form action="index.php" method="post">
+
+<input type="number" name="day" placeholder="Enter Number">
+<input type="text" value="<?php echo $result;?>" placholder="result" readonly>
+<input type="submit" value="submit">
+
+
+
+</form>
+     
+</body>
+</html>
+
+
+
+
