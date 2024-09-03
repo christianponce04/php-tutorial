3c5a6e12c8aaf10f2a52f2cf3d40b38ac5b79630
Christian <christianponce04@gmail.com>
2024-09-03 18:09:28 +0800

    exercise 3

--- index.php
+++ index.php
@@ -9,50 +9,49 @@
 
 <?php
 
-$day = $_POST["day"];
-$result = null;
 
-switch ($day) {
-    case "1":
-        $result = "It is Monday!";
-        break;
-    case "2":
-        $result = "It is today!";
-        break;
-    case "3":
-        $result = "It is Wednesday!";
-        break;
-	case "4":
-        $result = "It is Thursday!";
-        break;
-    case "5":
-        $result = "It is Friday!";
-        break;
-    case "6":
-        $result = "It is Saturday!";
-        break;
-	case "7":
-        $result = "It is Sunday!";
-        break;
-    default:
-        $result = "Invalid number!";
+//  for($i=0; $i < 8; $i++){
+//      for($star=0;$star < $i; $star++){
+//      echo "*" ;
+//      }
+//      echo "<br>";
+//  }
+
+for($i=8; $i > 0; $i--){
+    for($star=0;$star < $i; $star++){
+    echo "*" ;
+    }
+    echo "<br>";
 }
 
-?>
 
+
+?>
 <body>
 
-<!-- Write a program to show day of the week (for example: Monday) based on numbers using switch/case statements. -->
- 
-<form action="index.php" method="post">
+<!-- Write a Program to create following pattern using for loops:
 
-<input type="number" name="day" placeholder="Enter Number">
-<input type="text" value="<?php echo $result;?>" placholder="result" readonly>
-<input type="submit" value="submit">
+*
+**
+***
+****
+*****
+******
+*******
+******** -->
 
 
+<!--
+********
+*******
+******
+*****
+****
+***
+**
+* -->
 
-</form>
+
      
 </body>
 </html>
