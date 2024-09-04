1025e6b114e94743bec0f1fdb67bb2716c813dbe
Christian <christianponce04@gmail.com>
2024-09-04 16:06:44 +0800

    array

--- index.php
+++ index.php
@@ -1,49 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercises 4 BMI</title>
-</head>
+
 
 
 <?php
 
-    $weight = $_POST["weight"];
-    $height = $_POST["height"];
-    
-    
-    $result = $weight / ($height/100)**2;
-    $result = round($result,2);
+$foods = array("banana","watermelon","apple","orange");
 
-    if($result >= 30){
-        echo "Weight is{$result}  and you're obese";
-    }
-    else if($result >=25 && $result <=29.9){
-        echo "weight is  {$result} and you're overweight ";
-    }
-    else if($result >=18.5 && $result <= 24.9){
-        echo "weight is {$result}  and you're normal";
-    }
-    else{
-        echo "weight is {$result} and you're underweight";
-    }
+    array_push($foods ,"kiwi"); // add 
+    array_shift($foods); // remove first element
+    array_pop($foods); //remove last element 
+    echo count($foods) ."</br>" ;
+
+    // reverse an array
+
+    $reverse_array = array_reverse($foods);
+
+
+foreach ($foods as $food){
+    echo $food . "</br>";
+}   
+
+foreach ($reverse_array as $food){
+    echo $food . "</br>";
+}   
 
 ?>
-<body>
-
- <!-- Write a BMI Calculator -->
-
-    <form action="index.php" method="post">
-    <input type="number" name="height" id="" placeholder="Enter Height in CM" value="<?php echo $height ?>" >
-    <input type="number" name="weight" id="" placeholder="Enter Weight in KG" value="<?php echo $weight ?>" >
-    <input type="text" name="" id="" readonly placholder="result" value="<?php echo $result ?>">
-    <input type="submit" value="Submit">
-
-    </form>
-
-</body>
-</html>
 
 
 
