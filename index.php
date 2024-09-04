ae59d2e4b44ff3f214a23c1284c19b7bcfe48703
Christian <christianponce04@gmail.com>
2024-09-04 18:44:37 +0800

    isset and empty

--- index.php
+++ index.php
@@ -1,46 +1,44 @@
 
-
-
-<?php
-
-
-   // array_shift($countrys);
-    // array_pop($countrys);
-    // $countrys["USA"]="Las Vegas";
-    // $countrys["China"]="Beijing";
-
-    //   $keys = array_keys($countrys);
-    //   $values =  array_values($countrys);
-
-//    foreach($countrys as $key => $value){
-//      echo " {$key} = {$value} </br>";
-//    }         
-
-    $countrys = array("USA" => "Washington",
-                    "India" => "New Delhi",
-                    "Philippines" =>"Manila",
-                    "Korea"=>"Seoul");
-
-    $capital = $countrys[$_POST["capital"]];
-    
-    echo "the capital city is {$capital}";
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Associative Arrays</title>
+    <title>isset and empty</title>
 </head>
 <body>
 
-    <form action="index.php" method="POST">
-        <input type="text" name="capital" id="" placeholder="Enter Country">
-        <input type="submit" value="Submit">
-    </form>
+    <form action="index.php" method="post">
 
+    <input type="text" name="username" id="" placeholder="Enter Username">
+    <input type="password" name="password" id="" placeholder="Enter Password">
+    <input type="submit" value="Log In" name="login" >
+
+    </form>
+    
 </body>
 </html>
 
+<?php 
 
+// foreach ($_POST as $key => $value){
+//     echo "{$key} = {$value} <br>  ";
+// }
+
+    if(isset($_POST["login"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        
+        if(empty($username)){
+            echo "please input username";
+        }
+        else if(empty($password)){
+            echo "please input password";
+        }
+        else{
+            echo "hello {$username}";
+        }
+    }
+
+?>
 
