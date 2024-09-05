a77c36bf755e498117687c97723f5f2e80dfe6f1
Christian <christianponce04@gmail.com>
2024-09-05 18:32:33 +0800

    function

--- index.php
+++ index.php
@@ -1,74 +1,37 @@
 
+<?php 
+
+    $first_num = $_POST["first_num"];
+    $sec_num = $_POST["second_num"];
+
+    function add($a,$b){ return $result  = $a + $b ; }
+    function subtract($a,$b){ return $result  = $a - $b ; }
+    function multiply($a,$b){ return $result  = $a * $b ; }
+    function division($a,$b){ return $result  = $a / $b ; }
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Radio Button</title>
+    <title>function</title>
 </head>
-
-    <body>
+<body>
 
     <form action="index.php" method="post">
 
-        <input type="checkbox" name="pizza" id="" value="Pizza">Pizza <br>
-        <input type="checkbox" name="burger" id="" value="Burger">Burger <br>
-        <input type="checkbox" name="fries" id="" value="Fries">Fries <br>
-        <input type="checkbox" name="salad" id="" value="Salad">Salad <br>
-        <input type="submit"  value="submit" name="submit">
+        <input type="number" name="first_num" id="" placeholder="Enter First number">
+        <input type="number" name="second_num" id="" placeholder="Enter Second number">
+        <input type="number" name="result" id="" readonly value ="<?php echo add($first_num,$sec_num) ?>" >
+        <input type="submit" value="Submit">
 
     </form>
-
-    </body>
-
+    
+</body>
 </html>
 
-<?php 
-    
-    if(isset($_POST["submit"])){
-
-
-        if(isset($_POST["pizza"])){
-            echo "you like pizza" . "<br>";
-        }
-
-         else if (empty($_POST["fries"]) && empty($_POST["salad"]) && empty($_POST["burger"]) && empty($_POST["burger"]) ){
-            
-        echo "you dont like pizza,fries,salad and burger";
-      
-      }
-        
-    }
-
-
-
-    // if(isset($_POST["submit"])){
-     
-    //     if(isset($_POST["pizza"]))
-    //     {
-    //         echo "you like pizza";
-    //     }
-    //      if(isset($_POST["burger"]))
-    //     {
-    //         echo" you like burger";
-    //     }
-
-    //      if(isset($_POST["fries"]))
-    //     {
-    //         echo" you like fries";
-    //     }
-
-    //      if(isset($_POST["salad"]))
-    //     {
-    //         echo" you like salad";
-    //     }
-
-    //     else{
-    //         echo "Please select foods";
-    //     }
-
-    // }
-
-
-?>
 
