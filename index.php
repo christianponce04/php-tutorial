6de9919e1dfd1234582a237fdc3f1ea0634ba0b0
Christian <christianponce04@gmail.com>
2024-09-06 17:50:11 +0800

    continue function and adding switch condition

--- index.php
+++ index.php
@@ -4,11 +4,43 @@
     $first_num = $_POST["first_num"];
     $sec_num = $_POST["second_num"];
 
+    $result= $_POST["result"];
+
+    $operator= $_POST["operator"];
+
+    switch($operator){
+        
+        case $operator == 'Add':
+        
+            $result = add($first_num,$sec_num);
+            break;
+        
+        case $operator == 'Subtract':
+        
+            $result = subtract($first_num,$sec_num);
+                break;
+
+         case $operator == 'Multiply':
+        
+            $result = multiply($first_num,$sec_num);
+           break;
+
+
+           case $operator == 'Division':
+        
+            $result = division($first_num,$sec_num);
+              break;
+
+          default:
+        echo "invalid input";
+        break;
+        
+    }
+
     function add($a,$b){ return $result  = $a + $b ; }
     function subtract($a,$b){ return $result  = $a - $b ; }
     function multiply($a,$b){ return $result  = $a * $b ; }
     function division($a,$b){ return $result  = $a / $b ; }
-
 
 
 ?>
@@ -24,10 +56,14 @@
 
     <form action="index.php" method="post">
 
-        <input type="number" name="first_num" id="" placeholder="Enter First number">
-        <input type="number" name="second_num" id="" placeholder="Enter Second number">
-        <input type="number" name="result" id="" readonly value ="<?php echo add($first_num,$sec_num) ?>" >
-        <input type="submit" value="Submit">
+        <input type="number" name="first_num" id="" placeholder="Enter First number" value="<?php echo $first_num ?>" >
+        <input type="number" name="second_num" id="" placeholder="Enter Second number" value="<?php echo $sec_num ?>">
+        <input type="number" name="result" id="" readonly  value="<?php echo $result ?>">
+
+        <input type="submit" value="Add" name="operator">
+        <input type="submit" value="Subtract" name="operator">
+        <input type="submit" value="Multiply" name="operator">
+        <input type="submit" value="Division" name="operator">
 
     </form>
     
