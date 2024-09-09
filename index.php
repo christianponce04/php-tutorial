bae8cf663d4661678fd2f6424dfce10c6b6bb574
Christian Ponce <christianponce04@gmail.com>
2024-09-09 21:21:09 +0800

    input sanitize

--- index.php
+++ index.php
@@ -1,41 +1,53 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+
+</head>
+
+<body>
+
+    <form action="index.php" method="post">
+
+        username:<br>
+
+        <input type="text" name="username"><br>
+
+        age:<br>
+
+        <input type="text" name="age"><br>
+
+        email:<br>
+
+        <input type="text" name="email"><br>
+
+        <input type="submit" name="login" value="login"><br>
+
+    </form>
+
+</body>
+
+</html>
+
 <?php
 
-    $username = "Christian";
+    if(isset($_POST["login"])){
 
-    $phone = "123-456-7890";
-
-
-
-    //$username = strtolower($username);
-
-    //$username = strtoupper($username);
-
-    //$username = trim($username);
-
-    //$username = str_pad($username, 20, "0");
-
-    //$phone = str_replace("-", "", $phone);
-
-    //$username = strrev($username);
-
-    //$username = str_shuffle($username);
-
-    //$equals = strcmp($username, "Bro Code");
-
-    //$count = strlen($phone);
-
-    //$index = strpos($phone, "-");
-
-    //$firstname = substr($username, 0, 3);
-
-    //$lastname = substr($username, 4);
-
-    //$fullname = explode(" ", $username);
-
-    //$username = implode("-", $username);
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $age = filter_input(INPUT_POST, "age", FILTER_SANITIZE_NUMBER_INT);
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 
 
-
-    echo $username;
+        echo "Your email is: {$email}";
+    }
 
 ?>
