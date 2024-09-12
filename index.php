9bc1e051fc26f338298b14c6c75c47131a6818bf
Christian <christianponce04@gmail.com>
2024-09-12 19:09:24 +0800

    exercises & understanding the sanitize & validate

--- index.php
+++ index.php
@@ -1,50 +1,65 @@
 <!DOCTYPE html>
-
 <html lang="en">
-
 <head>
-
-    <meta charset="UTF-8">
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Document</title>
-
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
 </head>
-
 <body>
+	
 
-    <form action="index.php" method="post">
+<form action="index.php" method="post">
 
-      <br>
+<input type="text" name="username" id="" placeholder="Username">
+<input type="text" name="age" id="" placeholder="Age">
+<input type="text" name="email" id="" placeholder="Email">
+<input type="submit" value="Submit" name="submit">
 
-        <input type="text" name="username" placeholder="username"><br>
-
-        <input type="text" name="age" placeholder="age"><br>
-
-        <input type="text" name="email" placeholder="Email"><br>
-
-        <input type="submit" name="login" value="login"><br>
-
-    </form>
-
+</form>
 </body>
-
 </html>
 
-<?php
+<?php 
 
-    if(isset($_POST["login"])){
+if(isset($_POST["submit"])){
+	// $username = $_POST["username"];
+
+// 	 $username = filter_input(INPUT_POST, "username",
+// 	 				FILTER_SANITIZE_SPECIAL_CHARS);
+// 	$age = filter_input(INPUT_POST,"age",
+// 					FILTER_SANITIZE_NUMBER_INT);
+// $email = filter_input(INPUT_POST, "email",
+// 					FILTER_SANITIZE_EMAIL);
+// 	echo $email;
 
 
-        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-        $age = filter_input(INPUT_POST, "age", FILTER_SANITIZE_NUMBER_INT);
-        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-        $age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
-        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-        
-    }
+//validate
+
+// 	 if(isset($_POST["submit"])){
+// 	 	$age = filter_input(INPUT_POST,"age",FILTER_VALIDATE_INT);
+
+// 	 	if(empty($age)){
+// 	 		echo "invalid input";
+// 	 	}
+// 	 	else{
+// 	 		echo "your age is {$age}";
+// 	 	}
+// 	}
+// }
+
+	if(isset($_POST["submit"])){
+			$email = filter_input(INPUT_POST,"email",FILTER_VALIDATE_EMAIL);
+	
+			if(empty($email)){
+				echo "invalid input";
+			}
+			else{
+				echo "your email is {$email}";
+			}
+
+	}
+}
 
 ?>
+
+
