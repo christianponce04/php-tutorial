521d9ff0ce7ddc51b18db38de8e64e6e93aeb657
Christian <christianponce04@gmail.com>
2024-09-13 19:01:18 +0800

    adding new exercises for php

--- index.php
+++ index.php
@@ -1,21 +1,41 @@
-<table width="400px" cellspacing="0px" cellpadding="0px" border="1px">
+<!-- PHP Array to String Conversion (favourite colours chosen by user)  -->
+
+<html>
+<head>
+<title>
+array to string
+</title>
+</head>
+<body>
+	<form action="data.php" method="post">
+		Username: <input type="text" name="username" placeholder="enter name" required/><br/><br/>
+		Select your favourite colors:<br/>
+		Red<input type="checkbox" name="check_list[]" value="red"/><br/>
+		Blue<input type="checkbox" name="check_list[]" value="blue"/><br/>
+		Green<input type="checkbox" name="check_list[]" value="green"/><br/>
+		Yellow<input type="checkbox" name="check_list[]" value="yellow"/><br/>
+		Pink<input type="checkbox" name="check_list[]" value="pink"/><br/>
+		Black<input type="checkbox" name="check_list[]" value="black"/><br/>
+		White<input type="checkbox" name="check_list[]" value="white"/><br/><br/>
+		<input type="submit" name="submit" value="Submit"/><br/>
+	</form>
+</body>
+</html>
+
+Markup
+
 <?php
-for($row=1;$row<=8;$row++)
-{
-	echo "<tr>";
-	for($column=1;$column<=8;$column++)
-	{
-		$total=$row+$column;
-		if($total%2==0)
-		{
-			echo "<td height=35px width=30px bgcolor=#FFFFFF></td>";
-		}
-		else
-		{
-			echo "<td height=35px width=30px bgcolor=#000000></td>";
-		}
-	}
-	echo "</tr>";
+if (isset($_POST['submit'])) {
+    if (!empty($_POST['check_list'])) {
+        // Counting number of checked checkboxes.
+        $checked_count = count($_POST['check_list']);
+        $name = $_POST['username'];
+        echo $name . " 's favourite colors are " . $checked_count . " option(s): <br/>";
+        // Loop to store and display values of individual checked checkbox.
+        foreach ($_POST['check_list'] as $selected) {
+            echo "<p>" . $selected . "</p>";
+        }
+    } else {
+        echo "<b>Please Select Atleast One Option.</b>";
+    }
 }
-?>
-</table>
