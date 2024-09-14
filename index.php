62a53370d630add63baff55bcbd001b8047a8fd3
Christian Ponce <christianponce04@gmail.com>
2024-09-14 23:48:44 +0800

    includes

--- index.php
+++ index.php
@@ -1,39 +1,32 @@
-<html>
+<?php
+    include("header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>
-array to string
-</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-	<form action="data.php" method="post">
-		Username: <input type="text" name="username" placeholder="enter name" required/><br/><br/>
-		Select your favourite colors:<br/>
-		Red<input type="checkbox" name="check_list[]" value="red"/><br/>
-		Blue<input type="checkbox" name="check_list[]" value="blue"/><br/>
-		Green<input type="checkbox" name="check_list[]" value="green"/><br/>
-		Yellow<input type="checkbox" name="check_list[]" value="yellow"/><br/>
-		Pink<input type="checkbox" name="check_list[]" value="pink"/><br/>
-		Black<input type="checkbox" name="check_list[]" value="black"/><br/>
-		White<input type="checkbox" name="check_list[]" value="white"/><br/><br/>
-		<input type="submit" name="submit" value="Submit"/><br/>
-	</form>
+    This is the Home page<br>
+    Stuff about your home page can go here<br>
 </body>
 </html>
-
-Markup
-
 <?php
-if (isset($_POST['submit'])) {
-    if (!empty($_POST['check_list'])) {
-        // Counting number of checked checkboxes.
-        $checked_count = count($_POST['check_list']);
-        $name = $_POST['username'];
-        echo $name . " 's favourite colors are " . $checked_count . " option(s): <br/>";
-        // Loop to store and display values of individual checked checkbox.
-        foreach ($_POST['check_list'] as $selected) {
-            echo "<p>" . $selected . "</p>";
-        }
-    } else {
-        echo "<b>Please Select Atleast One Option.</b>";
-    }
-}
+    include("footer.html");
+?>
+
+<header>
+    <h2>This is my website</h2>
+    <a href="index.php">Home</a>
+    <a href="about.php">About</a>
+    <a href="locations.php">Locations</a>
+    <hr>
+</header>
+
+<footer>
+  
+    <a href="mailto:sample@gmail.com">sample@gmail.com</a>
+</footer>
