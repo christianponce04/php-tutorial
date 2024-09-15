e522f8a46e91022ed7a6e3024a4ea74aa12fb800
Christian Ponce <christianponce04@gmail.com>
2024-09-15 22:30:08 +0800

    validate exercise

--- index.php
+++ index.php
@@ -1,32 +1,16 @@
 <?php
-    include("header.html");
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    This is the Home page<br>
-    Stuff about your home page can go here<br>
-</body>
-</html>
-<?php
-    include("footer.html");
-?>
+// Specify an email address (valid or invalid)
+$email = "mail@example.com";
 
-<header>
-    <h2>This is my website</h2>
-    <a href="index.php">Home</a>
-    <a href="about.php">About</a>
-    <a href="locations.php">Locations</a>
-    <hr>
-</header>
-
-<footer>
-  
-    <a href="mailto:sample@gmail.com">sample@gmail.com</a>
-</footer>
+// Use the filter_var function with FILTER_VALIDATE_EMAIL to check if the email is valid
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) 
+{
+    // If the email is valid, print a message indicating it is valid
+    echo '"' . $email . '" = Valid'."\n";
+}
+else 
+{
+    // If the email is invalid, print a message indicating it is invalid
+    echo '"' . $email . '" = Invalid'."\n";
+}
+?>
