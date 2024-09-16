df6782edfadad6da517cc2a01478e3e6b9e18cdb
Christian <christianponce04@gmail.com>
2024-09-16 20:34:29 +0800

    include

--- about-us.php
+++ about-us.php
@@ -1,19 +1,17 @@
-<?php include("header.html") ?>
-<?php include("nav.html") ?>
+<?php include("header.html")?>
+<?php include("nav.html")?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>This is about us page</title>
 </head>
 <body>
 
-<h2>Hello this is body</h2>
+<h2>hello this is about us page</h2>
     
 </body>
 </html>
-
-
-<?php include("footer.html") ?>
+<?php include("footer.html")?>
