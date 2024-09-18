350cee31dd8afa80fdcd65c7fbb3f49086edfb7d
Christian <christianponce04@gmail.com>
2024-09-18 20:45:26 +0800

    exercises recursive

--- index.php
+++ index.php
@@ -1,41 +1,14 @@
-<!-- PHP Array to String Conversion (favourite colours chosen by user)  -->
-
-<html>
-<head>
-<title>
-array to string
-</title>
-</head>
-<body>
-	<form action="data.php" method="post">
-		Username: <input type="text" name="username" placeholder="enter name" required/><br/><br/>
-		Select your favourite colors:<br/>
-		Red<input type="checkbox" name="check_list[]" value="red"/><br/>
-		Blue<input type="checkbox" name="check_list[]" value="blue"/><br/>
-		Green<input type="checkbox" name="check_list[]" value="green"/><br/>
-		Yellow<input type="checkbox" name="check_list[]" value="yellow"/><br/>
-		Pink<input type="checkbox" name="check_list[]" value="pink"/><br/>
-		Black<input type="checkbox" name="check_list[]" value="black"/><br/>
-		White<input type="checkbox" name="check_list[]" value="white"/><br/><br/>
-		<input type="submit" name="submit" value="Submit"/><br/>
-	</form>
-</body>
-</html>
-
-Markup
+<!-- Factorial program in PHP using recursive function  -->
 
 <?php
-if (isset($_POST['submit'])) {
-    if (!empty($_POST['check_list'])) {
-        // Counting number of checked checkboxes.
-        $checked_count = count($_POST['check_list']);
-        $name = $_POST['username'];
-        echo $name . " 's favourite colors are " . $checked_count . " option(s): <br/>";
-        // Loop to store and display values of individual checked checkbox.
-        foreach ($_POST['check_list'] as $selected) {
-            echo "<p>" . $selected . "</p>";
-        }
+function factorial($number) {
+
+    if ($number < 2) {
+        return 1;
     } else {
-        echo "<b>Please Select Atleast One Option.</b>";
+        return ($number * factorial($number-1));
     }
 }
+
+echo factorial(4);
+?>
