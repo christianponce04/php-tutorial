1550f9be739dd784dc6b5575cbebe17a59117a2e
Christian <christianponce04@gmail.com>
2024-09-19 20:45:17 +0800

    exercise using ternary

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
-<table width="400px" cellspacing="0px" cellpadding="0px" border="1px">
 <?php
-for($row=1;$row<=8;$row++)
-{
-	echo "<tr>";
-	for($column=1;$column<=8;$column++)
-	{
-		$total=$row+$column;
-		if($total%2==0)
-		{
-			echo "<td height=35px width=30px bgcolor=#FFFFFF></td>";
-		}
-		else
-		{
-			echo "<td height=35px width=30px bgcolor=#000000></td>";
-		}
-	}
-	echo "</tr>";
+// Function to test a given number using ternary operators
+function trinary_Test($n){
+    // Ternary operators used to check the value of $n and assign a corresponding message to $r
+    $r = $n > 30
+        ? "greater than 30"
+        : ($n > 20
+            ? "greater than 20"
+            : ($n > 10
+                ? "greater than 10"
+                : "Input a number at least greater than 10!"));
+
+    // Display the result with the input number
+    echo $n . " : " . $r . "\n";
 }
+
+// Test the function with different input values
+trinary_Test(32) ;
+trinary_Test(21);
+trinary_Test(12);
+trinary_Test(4);
 ?>
-</table>
