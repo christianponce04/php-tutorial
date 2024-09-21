008097b28eeb3bddecec51024447256181a01733
Christian Ponce <christianponce04@gmail.com>
2024-09-21 23:42:58 +0800

    exercises

--- index.php
+++ index.php
@@ -1,49 +1,10 @@
-<?php 
-session_start();
-?>
+<?php
+$count = 5;
+while($count <= 15)
+{
+  echo $count;
+  echo "<br>" ;
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Session Discussion</title>
-</head>
-
-<body>
-
-<h2>this is login page</h2>
-
-<form action="index.php" method="POST">
-    <input type="text" name="username" id="" placeholder="Username">
-    <input type="password" name="password" id="" placeholder="password">
-    <input type="submit" value="login" name="login">
-
-</form>
-   
-</body>
-</html>
-
-
-<?php 
-
-if(isset($_POST["login"])){
-
-    if(!empty($_POST["username"]) && !empty($_POST["password"]) ){
-
-    $_SESSION["username"] = $_POST["username"];
-    $_SESSION["password"] = $_POST["password"];
-
-
-    header("Location: home.php");
- 
+  $count++;
 }
-else{
-    echo "please input username/password";
-}
-
-
-}
-
-
 ?>
