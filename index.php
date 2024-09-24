3474bc461fb31f3edde88e3d8495cf8ab20a51aa
Christian <christianponce04@gmail.com>
2024-09-24 20:29:21 +0800

    db connection

--- index.php
+++ index.php
@@ -1,35 +1,17 @@
-
- <!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Server Super global variables</title>
- </head>
- <body>
-
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="POST">
-
-    <input type="text" name="username" id="" placeholder="Username">
-    <input type="submit" value="submit">
-
-    </form>
-        
- </body>
- </html>
-
 <?php 
+   include("database.php")
+?> 
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    echo "Hello {$_POST["username"]} ";
-}
-else{
-    echo "input please";
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Database connection</title>
+</head>
+<body>
 
-
-// foreach($_SERVER as $key => $value){
-//     echo "{$key} = {$_value} <br>" ;
-// }
-
-?>
+<h2>Hello Database</h2>
+   
+</body>
+</html>
