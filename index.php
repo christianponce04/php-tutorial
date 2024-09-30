2f05fcddd33431624c85a839d2cf087cdc187988
Christian <christianponce04@gmail.com>
2024-09-30 20:14:25 +0800

    electricity bill exercise

--- index.php
+++ index.php
@@ -1,22 +1,62 @@
-<?php
-// Function to test a given number using ternary operators
-function trinary_Test($n){
-    // Ternary operators used to check the value of $n and assign a corresponding message to $r
-    $r = $n > 30
-        ? "greater than 30"
-        : ($n > 20
-            ? "greater than 20"
-            : ($n > 10
-                ? "greater than 10"
-                : "Input a number at least greater than 10!"));
+<!DOCTYPE html>
 
-    // Display the result with the input number
-    echo $n . " : " . $r . "\n";
+<head>
+	<title>PHP - Calculate Electricity Bill</title>
+</head>
+
+<?php
+$result_str = $result = '';
+if (isset($_POST['unit-submit'])) {
+    $units = $_POST['units'];
+    if (!empty($units)) {
+        $result = calculate_bill($units);
+        $result_str = 'Total amount of ' . $units . ' - ' . $result;
+    }
+}
+/**
+ * To calculate electricity bill as per unit cost
+ */
+function calculate_bill($units) {
+    $unit_cost_first = 3.50;
+    $unit_cost_second = 4.00;
+    $unit_cost_third = 5.20;
+    $unit_cost_fourth = 6.50;
+
+    if($units <= 50) {
+        $bill = $units * $unit_cost_first;
+    }
+    else if($units > 50 && $units <= 100) {
+        $temp = 50 * $unit_cost_first;
+        $remaining_units = $units - 50;
+        $bill = $temp + ($remaining_units * $unit_cost_second);
+    }
+    else if($units > 100 && $units <= 200) {
+        $temp = (50 * 3.5) + (100 * $unit_cost_second);
+        $remaining_units = $units - 150;
+        $bill = $temp + ($remaining_units * $unit_cost_third);
+    }
+    else {
+        $temp = (50 * 3.5) + (100 * $unit_cost_second) + (100 * $unit_cost_third);
+        $remaining_units = $units - 250;
+        $bill = $temp + ($remaining_units * $unit_cost_fourth);
+    }
+    return number_format((float)$bill, 2, '.', '');
 }
 
-// Test the function with different input values
-trinary_Test(32) ;
-trinary_Test(21);
-trinary_Test(12);
-trinary_Test(4);
 ?>
+
+<body>
+	<div id="page-wrap">
+		<h1>Php - Calculate Electricity Bill</h1>
+
+		<form action="" method="post" id="quiz-form">
+            	<input type="number" name="units" id="units" placeholder="Please enter no. of Units" />
+            	<input type="submit" name="unit-submit" id="unit-submit" value="Submit" />
+		</form>
+
+		<div>
+		    <?php echo '<br />' . $result_str; ?>
+		</div>
+	</div>
+</body>
+</html>
