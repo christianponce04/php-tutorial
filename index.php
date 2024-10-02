dadc59b82cb394971a4079b73bb2739116d46b01
Christian <christianponce04@gmail.com>
2024-10-02 16:29:28 +0800

    updates

--- index.php
+++ index.php
@@ -7,4 +7,16 @@ while($count <= 15)
 
   $count++;
 }
-?>
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
